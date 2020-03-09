2b7e1dcc72325c2c787a9900d0acf286fe0dfaef
xfding <xfding@vw-mobvoi.com>
2020-03-09 18:25:22 +0800

    add vwm script

--- egs/asr/s5a/local/run_cleanup_segmentation.sh
+++ egs/asr/s5a/local/run_cleanup_segmentation.sh
@@ -22,12 +22,13 @@ set -o pipefail
 set -u
 
 out_dir=/home/data/xfding/train_result/asr/multi
+test_enable=false
 stage=0
 cleanup_stage=0
 data=$out_dir/data/train_all
 cleanup_affix=cleaned
 srcdir=$out_dir/exp/tri4a
-nj=100
+nj=5
 decode_nj=10
 decode_num_threads=4
 test_sets=""
@@ -45,30 +46,31 @@ cleaned_dir=${srcdir}_${cleanup_affix}
 if [ $stage -le 1 ]; then
 	# This does the actual data cleanup.
 	steps/cleanup/clean_and_segment_data.sh --stage $cleanup_stage --nj $nj --cmd "$train_cmd" \
-		$data $out_dir/data/lang $srcdir $dir $cleaned_data
+		$data $out_dir/data/lang $srcdir $dir $cleaned_data || exit 1;
 fi
 
 if [ $stage -le 2 ]; then
 	steps/align_fmllr.sh --nj $nj --cmd "$train_cmd" \
-		$cleaned_data $out_dir/data/lang $srcdir ${srcdir}_ali_${cleanup_affix}
+		$cleaned_data $out_dir/data/lang $srcdir ${srcdir}_ali_${cleanup_affix} || exit 1;
 fi
 
 if [ $stage -le 3 ]; then
 	steps/train_sat.sh --cmd "$train_cmd" \
-		12000 190000 $cleaned_data $out_dir/data/lang ${srcdir}_ali_${cleanup_affix} ${cleaned_dir}
+		12000 190000 $cleaned_data $out_dir/data/lang ${srcdir}_ali_${cleanup_affix} ${cleaned_dir} || exit 1;
 fi
 
 if [ $stage -le 4 ]; then
 	# Test with the models trained on cleaned-up data.
-	utils/mkgraph.sh $out_dir/data/lang_combined_tg ${cleaned_dir} ${cleaned_dir}/graph_tg
-
-#	for c in $test_sets; do
-#	(
-#		steps/decode_fmllr.sh --nj $decode_nj --num-threads $decode_num_threads \
-#        		--cmd "$decode_cmd" \
-#        		${cleaned_dir}/graph_tg $out_dir/data/${c}/test ${cleaned_dir}/decode_${c}_tg
-#   	) &
-#  	done
+	utils/mkgraph.sh $out_dir/data/lang_combined_tg ${cleaned_dir} ${cleaned_dir}/graph_tg || exit 1;
+	if $test_enable; then
+		for c in $test_sets; do
+		(
+			steps/decode_fmllr.sh --nj $decode_nj --num-threads $decode_num_threads \
+				--cmd "$decode_cmd" \
+				${cleaned_dir}/graph_tg $out_dir/data/${c}/test ${cleaned_dir}/decode_${c}_tg
+		) &
+		done
+	fi
 fi
 
 wait;
---
+++ egs/asr/s5a/local/vwm_format.py
@@ -0,0 +1,38 @@
+import sys,os
+import re
+from shutil import copyfile
+
+vwm_dir=sys.argv[1]
+out_dir=sys.argv[2]
+
+vwm_script = os.path.join(vwm_dir, 'script')
+out_script = os.path.join(out_dir, 'script')
+if(not os.path.isdir(out_script)):
+    os.makedirs(out_script)
+
+vwm_wav_dir = os.path.join(vwm_dir,'wav')
+out_wav_dir = os.path.join(out_dir,'wav')
+if(not os.path.isdir(out_wav_dir)):
+    os.makedirs(out_wav_dir)
+
+script = open(os.path.join(vwm_script,'script.txt'))
+new_script = open(os.path.join(out_script, 'script.txt'),"w")
+
+for line in script.readlines():
+    wav,trans = line.replace(' ','\t').split('\t',1)
+    s = wav.split('_')[1:]
+    s[0]=s[0].zfill(4)
+    new_name = "_".join(s)
+
+    new_path = os.path.join(out_wav_dir, new_name)
+    old_path = os.path.join(vwm_wav_dir, wav)
+    if os.path.isfile(old_path):
+        copyfile(old_path, new_path)
+        new_script.write(new_name + " " + trans)
+    else:
+        print (old_path + " not exist")
+
+script.close()
+new_script.close()
+
+print ("All done!")
